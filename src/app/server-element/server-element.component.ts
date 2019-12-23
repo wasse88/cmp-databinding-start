7d6b55410c0d410907f08ff2e9acadb5ae9a34fe
Avi Wasse <aviws198888@gmail.com>
2019-12-23 17:15:35 +0200

    Update server-element.component.ts

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +19,53 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //None, Native, ShadowDom, Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('constructor log');
+   }
+  
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges log');
+    console.log( changes);
   }
 
+  ngOnInit() {
+    console.log('ngOnInit log');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck log');
+  }
+
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit log');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked log');
+  }
+
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit log');
+  }
+
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked log');
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy log');
+  }
 }
+ 
